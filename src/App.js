e00d0fd691fddd276c9a2cf8e66a2e9aaf1db18a
Brian Bycynski <high5bri@hotmail.com>
2017-12-15 07:19:57 -0500

    finished lecture 7

--- src/App.js
+++ src/App.js
@@ -2,7 +2,25 @@ import React, { Component } from 'react'
 
 class App extends Component {
     render() {
-        return <header className="header">some text</header>
+        return (
+            <header className="header">
+                <div className="logo-box">
+                    <img
+                        src={require('./img/logo-white.png')}
+                        alt="logo"
+                        className="logo"
+                    />
+                </div>
+                <div className="text-box">
+                    <h1 className="heading-primary">
+                        <span className="heading-primary-main">Outdoors</span>
+                        <span className="heading-primary-sub">
+                            is where life happens
+                        </span>
+                    </h1>
+                </div>
+            </header>
+        )
     }
 }
 
