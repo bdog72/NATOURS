a043fc7c914fab8697419562201ca3f482311c13
Brian Bycynski <high5bri@hotmail.com>
2017-12-17 09:58:34 -0500

    finished lecture 117

--- src/App.js
+++ src/App.js
@@ -4,20 +4,23 @@ class App extends Component {
     render() {
         return (
             <header className="header">
-                <div className="logo-box">
+                <div className="header__logo-box">
                     <img
                         src={require('./img/logo-white.png')}
                         alt="logo"
-                        className="logo"
+                        className="header__logo"
                     />
                 </div>
-                <div className="text-box">
+                <div className="header__text-box">
                     <h1 className="heading-primary">
-                        <span className="heading-primary-main">Outdoors</span>
-                        <span className="heading-primary-sub">
+                        <span className="heading-primary--main">Outdoors</span>
+                        <span className="heading-primary--sub">
                             is where life happens
                         </span>
                     </h1>
+                    <a href="" className="btn btn--white btn--animated">
+                        Discover Our Tours
+                    </a>
                 </div>
             </header>
         )
