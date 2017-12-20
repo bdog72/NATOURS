fac39229e82380b0db7a0408e77f705b15b32462
Brian Bycynski <high5bri@hotmail.com>
2017-12-20 06:34:10 -0500

    finished lecture 34

--- src/App.js
+++ src/App.js
@@ -3,26 +3,30 @@ import React, { Component } from 'react'
 class App extends Component {
     render() {
         return (
-            <header className="header">
-                <div className="header__logo-box">
-                    <img
-                        src={require('./img/logo-white.png')}
-                        alt="logo"
-                        className="header__logo"
-                    />
-                </div>
-                <div className="header__text-box">
-                    <h1 className="heading-primary">
-                        <span className="heading-primary--main">Outdoors</span>
-                        <span className="heading-primary--sub">
-                            is where life happens
-                        </span>
-                    </h1>
-                    <a href="" className="btn btn--white btn--animated">
-                        Discover Our Tours
-                    </a>
-                </div>
-            </header>
+            <div>
+                <header className="header">
+                    <div className="header__logo-box">
+                        <img
+                            src={require('./img/logo-white.png')}
+                            alt="logo"
+                            className="header__logo"
+                        />
+                    </div>
+                    <div className="header__text-box">
+                        <h1 className="heading-primary">
+                            <span className="heading-primary--main">
+                                Outdoors
+                            </span>
+                            <span className="heading-primary--sub">
+                                is where life happens
+                            </span>
+                        </h1>
+                        <a href="" className="btn btn--white btn--animated">
+                            Discover Our Tours
+                        </a>
+                    </div>
+                </header>
+            </div>
         )
     }
 }
